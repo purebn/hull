9ecfb4c0c9b4e5e6fc2c20dfe3708796a4daf9da
imanfani <iman.farsi.hacker@gmail.com>
2016-02-27 17:43:56 +0330

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -115,18 +115,18 @@ end
 return {
   description = "Know your id or the id of a chat members.",
   usage = {
-    "!id: Return your ID and the chat id if you are in one.",
-    "!ids chat: Return the IDs of the current chat members.",
-    "!ids chat <chat_id>: Return the IDs of the <chat_id> members.",
-    "!id member @<user_name>: Return the member @<user_name> ID from the current chat",
-    "!id members name <text>: Search for users with <text> on first_name, print_name or username on current chat"
+    "id: Return your ID and the chat id if you are in one.",
+    "ids chat: Return the IDs of the current chat members.",
+    "ids chat <chat_id>: Return the IDs of the <chat_id> members.",
+    "id member @<user_name>: Return the member @<user_name> ID from the current chat",
+    "id members name <text>: Search for users with <text> on first_name, print_name or username on current chat"
   },
   patterns = {
-    "^!id$",
-    "^!ids? (chat) (%d+)$",
-    "^!ids? (chat)$",
-    "^!id (member) (@)(.+)",
-    "^!id (members) (name) (.+)"
+    "^[!/#$&@ ]id$",
+    "^[!/#$&@ ]ids? (chat) (%d+)$",
+    "^[!/#$&@ ]ids? (chat)$",
+    "^[!/#$&@ ]id (member) (@)(.+)",
+    "^[!/#$&@ ]id (members) (name) (.+)"
   },
   run = run
 }
