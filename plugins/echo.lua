04fe0cf168bf7019815478f2af693a43ad3a3afa
imanfani <iman.farsi.hacker@gmail.com>
2016-02-27 17:38:46 +0330

    Update echo.lua

--- plugins/echo.lua
+++ plugins/echo.lua
@@ -12,9 +12,9 @@ end
 
 return {
   description = "Simplest plugin ever!",
-  usage = "!echo [whatever]: echoes the msg",
+  usage = "echo [whatever]: echoes the msg",
   patterns = {
-    "^!echo +(.+)$"
+    "^[!/#$&@ ]echo +(.+)$"
   }, 
   run = run 
 }
